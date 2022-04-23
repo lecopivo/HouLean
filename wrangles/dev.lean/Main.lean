4096909647f6cf36d72ede6f2b85819784ef7708
lecopivo <skrivantomas@seznam.cz>
2022-04-23 21:31:08 +0200

    ...

---
+++ wrangles/dev.lean/Main.lean
@@ -0,0 +1,11 @@
+import HouLean
+
+def main : IO Unit := do
+
+  let time ← Hou.time
+
+  for i in [0:(← Hou.npoints 0).toNat] do
+    let mut p ← Hou.getPointV3 0 "P" i.toUInt64
+    let R := Float.sqrt (p.x * p.x + p.z * p.z)
+    p := {p with y := p.y + 0.1 * Float.sin (10 * R - 4 * time)}
+    Hou.setPointV3 "P" i.toUInt64 p
